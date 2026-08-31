4c532c9b345a00ea8af4141ef071160e35463248
ICO KAL <kalconcepcion031@gmail.com>
2026-08-31 18:35:15 -0400

    implementacion de la contrase;a

--- sistemaReservasGestions/src/frontend/login.jsx
+++ sistemaReservasGestions/src/frontend/login.jsx
@@ -1,7 +1,24 @@
-import React from "react";
+import { MongoClient } from "mongodb";
+import React, { useState } from "react";
+const mongo = require('../backend/reservasGestionsBD');
 
 function login(){
-
+     const [contraseña, setContraseña] = useState('');
+     const coneection = mongo();
+     // function de contrase;a
+     const handleChange = (event) => {
+         setContraseña(event.target.value);
+     };
+     return (
+         <div>
+             <input 
+                 type="password" 
+                 value={contraseña} 
+                 onChange={handleChange} 
+             />
+         </div>
+     );
+    
 }
 
 export default login;
